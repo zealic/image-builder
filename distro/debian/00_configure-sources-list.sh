e099989f3b8615908e1d073df20a53290ede51f0
zealic <zealic@gmail.com>
2019-02-22 13:20:59 +0800

    * Move distro scripts to distro directory.

--- distro/debian/00_configure-sources-list.sh
+++ distro/debian/00_configure-sources-list.sh
@@ -1,6 +1,8 @@
+cat > /etc/apt/sources.list <<"EOF"
 deb http://deb.debian.org/debian/ stretch main
 deb-src http://deb.debian.org/debian/ stretch main
 deb http://security.debian.org/ stretch/updates main
 deb-src http://security.debian.org/ stretch/updates main
 deb http://deb.debian.org/debian/ stretch-updates main
 deb-src http://deb.debian.org/debian/ stretch-updates main
+EOF
