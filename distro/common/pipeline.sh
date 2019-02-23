be63e0541b4864cacd27ab8d5c514b602f54f888
zealic <zealic@gmail.com>
2019-02-23 16:35:38 +0800

    * Sect constants.

--- distro/common/pipeline.sh
+++ distro/common/pipeline.sh
@@ -23,8 +23,8 @@ qemu-nbd -t -c $NBD_DEV ${IMAGE_FILE}
 
 # Make parts
 parted $NBD_DEV "mklabel msdos"
-parted $NBD_DEV "mkpart primary ext4 4096s -1"
+parted $NBD_DEV "mkpart primary ext4 ${SYSIMG_SECT_COUNT}s -1"
 parted $NBD_DEV set 1 boot on
 PART_SEC_COUNT=$(fdisk -l $NBD_DEV | grep ${NBD_DEV}p1 | awk '{ print $5}')
-losetup -o $SYSIMG_SIZE --sizelimit $((PART_SEC_COUNT*512)) $LOOP_DEV $NBD_DEV
+losetup -o $SYSIMG_SIZE --sizelimit $(($PART_SEC_COUNT*$SECT_SIZE)) $LOOP_DEV $NBD_DEV
 yes | mkfs.ext4 -U $MAIN_UUID $LOOP_DEV
