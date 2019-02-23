ed192f233cf458bb2f6a8fbb49a0e8025ec9736a
zealic <zealic@gmail.com>
2019-02-23 14:49:57 +0800

    * Improve networ config.

--- distro/debian/packages/01_basic.sh
+++ distro/debian/packages/01_basic.sh
@@ -6,8 +6,16 @@ apt-get install -yq \
   git openssh-server
 
 cat >> /etc/network/interfaces <<EOF
+# The dhcp address
 auto eth0
 iface eth0 inet dhcp
+
+# The static address
+#iface eth0 inet static
+#	address 10.0.0.200
+#	netmask 255.255.0.0
+#	gateway 10.0.0.1
+#	dns-nameservers 10.0.0.1
 EOF
 
 systemctl enable ssh
