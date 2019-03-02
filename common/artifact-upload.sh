210095cb972c35553898e38c16b85263651c04c9
zealic <zealic@gmail.com>
2019-03-02 16:24:16 +0800

    * Less info for upload artifacts.

--- common/artifact-upload.sh
+++ common/artifact-upload.sh
@@ -2,4 +2,4 @@ UPLOAD_HOST=${UPLOAD_HOST:-RZ-DSM:5005}
 UPLOAD_URL=http://$UPLOAD_HOST/build/distro/$(basename $TARGET_FILE)
 
 echo Uploading $(basename $TARGET_FILE) ...
-curl -u anonymous: -T "$TARGET_FILE" $UPLOAD_URL
+curl -sS -u anonymous: -T "$TARGET_FILE" $UPLOAD_URL
