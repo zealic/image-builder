ee46c5d12f9337e06f1afd26b1066d02a97368a7
zealic <zealic@gmail.com>
2019-03-02 18:48:18 +0800

    * Auto create webdav upload directory.

--- common/artifact-upload.sh
+++ common/artifact-upload.sh
@@ -1,5 +1,8 @@
 UPLOAD_HOST=${UPLOAD_HOST:-RZ-DSM:5005}
-UPLOAD_URL=http://$UPLOAD_HOST/build/distro/$(basename $TARGET_FILE)
+UPLOAD_URL=http://$UPLOAD_HOST/build/distro/$(date +%Y-%m)
+UPLOAD_FILE=$UPLOAD_URL/$(basename $TARGET_FILE)
 
+# WebDAV upload
 echo Uploading $(basename $TARGET_FILE) ...
-curl -sS -u anonymous: -T "$TARGET_FILE" $UPLOAD_URL
+curl -sS -u anonymous: -X MKCOL UPLOAD_URL
+curl --fail -sS -u anonymous: -T "$TARGET_FILE" $UPLOAD_URL
