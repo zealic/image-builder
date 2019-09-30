8c7938028866b4d04fbf79d7953ea25932d7e2a0
zealic <zealic@gmail.com>
2019-09-30 15:46:01 +0800

    * Full disable IPv6 and enable net.bridge.bridge-nf-call-iptables.

--- distro/debian/config/04_sysctl.sh
+++ distro/debian/config/04_sysctl.sh
@@ -1,13 +1,20 @@
-cat > /etc/sysctl.d/40-ip.conf <<EOF
-# Enable IPv4 IP forward
-net.ipv4.ip_forward = 1
-# Disable IPv6
-net.ipv6.conf.all.disable_ipv6 = 1
-EOF
-
 cat > /etc/security/limits.d/10-files.conf <<EOF
 root soft nofile 65535
 root hard nofile 65535
 * soft nofile 65535
 * hard nofile 65535
+EOF
+
+cat > /etc/sysctl.d/40-ip.conf <<EOF
+# Enable IPv4 IP forward
+net.ipv4.ip_forward = 1
+# Disable IPv6
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+
+cat > /etc/sysctl.d/41-network.conf <<EOF
+# for Docker and Romana
+net.bridge.bridge-nf-call-iptables=1
 EOF
