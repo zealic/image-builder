16201878cd1bf679a2ef230f0d49fbf99bbeef0a
zealic <zealic@gmail.com>
2019-10-29 07:58:20 +0000

    + OpenWRT editions.

---
+++ distro/openwrt/@edition/client/software.sh
@@ -0,0 +1,5 @@
+opkg update
+opkg install ChinaDNS luci-app-chinadns
+opkg install dns-forwarder luci-app-dns-forwarder
+opkg install shadowsocks-libev luci-app-shadowsocks simple-obfs
+opkg install ShadowVPN luci-app-shadowvpn
---
+++ distro/openwrt/@edition/server/software.sh
@@ -0,0 +1,4 @@
+opkg update
+opkg install ShadowVPN luci-app-shadowvpn
+opkg install shadowsocks-libev-server simple-obfs-server
+opkg install luci-app-ocserv ocserv openconnect luci-proto-openconnect
