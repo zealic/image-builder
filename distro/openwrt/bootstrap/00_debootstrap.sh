b65536130d829e4e551749903f878b7d28a4fa84
vagrant <vagrant@RZ-BTON>
2020-04-28 04:35:31 +0000

    Fix openwrt image url

--- distro/openwrt/bootstrap/00_debootstrap.sh
+++ distro/openwrt/bootstrap/00_debootstrap.sh
@@ -5,7 +5,7 @@
 OPENWRT_VER=19.07
 OPENWRT_URL=https://downloads.openwrt.org/releases/${OPENWRT_VER}/targets/x86/64/openwrt-${OPENWRT_VER}-x86-64-generic-rootfs.tar.gz
 VMLINUZ_URL=https://downloads.openwrt.org/releases/${OPENWRT_VER}/targets/x86/64/openwrt-${OPENWRT_VER}-x86-64-vmlinuz
-OPENWRT_URL=https://downloads.openwrt.org/snapshots/targets/x86/64/openwrt-x86-64-generic-rootfs.tar.gz
+OPENWRT_URL=https://downloads.openwrt.org/snapshots/targets/x86/64/openwrt-x86-64-rootfs.tar.gz
 VMLINUZ_URL=https://downloads.openwrt.org/snapshots/targets/x86/64/openwrt-x86-64-vmlinuz
 
 mkdir -p $TARGET_DIR/boot
