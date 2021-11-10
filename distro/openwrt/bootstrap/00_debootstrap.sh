b7ca5e9493cb7b5765b3d2b64c4c7a5d4186f54f
Zealic <zealic@gmail.com>
2021-11-10 15:13:57 +0800

    upgrade: Upgrade OpenWRT to 21.02.1

--- distro/openwrt/bootstrap/00_debootstrap.sh
+++ distro/openwrt/bootstrap/00_debootstrap.sh
@@ -2,11 +2,9 @@
 #===============================================================================
 # debootstrap install
 #===============================================================================
-OPENWRT_VER=19.07.3
+OPENWRT_VER=21.02.1
 OPENWRT_URL=https://downloads.openwrt.org/releases/${OPENWRT_VER}/targets/x86/64/openwrt-${OPENWRT_VER}-x86-64-generic-rootfs.tar.gz
 VMLINUZ_URL=https://downloads.openwrt.org/releases/${OPENWRT_VER}/targets/x86/64/openwrt-${OPENWRT_VER}-x86-64-vmlinuz
-OPENWRT_URL=https://downloads.openwrt.org/snapshots/targets/x86/64/openwrt-x86-64-rootfs.tar.gz
-VMLINUZ_URL=https://downloads.openwrt.org/snapshots/targets/x86/64/openwrt-x86-64-generic-kernel.bin
 
 mkdir -p $TARGET_DIR/boot
 curl -sSL $OPENWRT_URL | tar -C $TARGET_DIR -xvzf - > /dev/null
